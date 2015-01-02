21f4672ca04ccb489187e08fa4e42b19f3a4d9dd
Emma Grasmeder <e.grasmeder@gmail.com>
2015-01-02 12:26:43 -0500

    added another possible way to manage the algorithm

---
+++ todo-more/TodoFile.py
@@ -0,0 +1,24 @@
+class TodoFile():
+    '''
+        Opens, reads in, appends to, and generally manages
+        communication between todo-more logic and the todo-file.
+    '''
+    def __init__():
+        pass
+    def sort():
+        '''
+            Re-arrange items according to listed categories
+        '''
+        pass
+    def find():
+        '''
+            Returns record based on search term
+        '''
+        pass
+    def delete():
+        '''
+            Permanently deletes item, requires input: 
+                "delete item [index_number]" 
+        '''
+        pass
+    
