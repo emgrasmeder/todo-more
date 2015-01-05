2e16be89f3566c8b0047d152005c7b1e6d55b3c2
Emma Grasmeder <e.grasmeder@gmail.com>
2015-01-05 17:29:00 -0500

    added capability to check off completed items

--- todo-more/TodoFile.py
+++ todo-more/TodoFile.py
@@ -9,12 +9,13 @@ class TodoFile():
         An instance of TodoFile is created when user runs the 
         Python file. 
     '''
-    def __init__(self, debug=False, todo_file="default"):
+    def __init__(self, debug=False, todo_file="default",has_headers=True):
         if todo_file == "default":
             filename = "sorted_ideas.csv"
             path = "/home/emma/Dropbox/"
             self.debug=debug
             self.cleaned_file = False
+            self.has_headers = has_headers
             self.headers = ["ID",
                             "Importance",
                             "StartDate",
@@ -28,52 +29,39 @@ class TodoFile():
             with open(self.fullpath) as f:
                 self.todo_file = [line.split('\t') for line in f]
             
-            self.start_date_index= self.get_header_index(header="StartDate")
-            self.index_index = self.get_header_index(header="ID")
+            self.start_date_index= self.get_header_index("StartDate")
+            self.index_index = self.get_header_index("ID")
         else:
             raise Exception("TodoFile can't handle any arguments yet.")
 
-    def get_header_index(self,header=False):
+    def get_header_index(self,header):
         if header:
             index = [index 
                         for index,item in enumerate(self.headers) 
                             if header == item]
             return(index[0])
         else:
-            raise InputError("get_header_index requires a header to find")
-
-    def get_record(self, record_id=False):
-        if record_id != "max":
-            record = list(filter(
-                        lambda record: 
-                            int(record[self.get_header_index(header="ID")]
-                                )==record_id, self.cleaned_file))
-            print(record)
-        else: #if record_id = "max"
-            if not self.cleaned_file:
-                self.cleaned_file = self.clean_file()
-                max_record_id = list(map(lambda record: 
-                    int(record[self.get_header_index(header="ID")]),
-                        self.cleaned_file))
-                return(int(max(max_record_id)))
+            input("Argument Error: get_header_index can't handle headerless\
+                data sets yet")
      
-    def clean_file(self, headers=True):
+    def clean_file(self):
         '''
             Cleaned File is the input file without headers, training newlines
             or blank entries (based on input date, which should always exist)
         '''
-        if headers:
-            return(list(filter(lambda record: 
-                        record[self.get_header_index(header="StartDate")]!="",
-                            self.todo_file[1:])))
+        if self.has_headers:
+            loc_cleaned_file = list(filter(lambda record: 
+                        record[self.get_header_index("StartDate")]!="",
+                            self.todo_file[1:]))
+            return(loc_cleaned_file)
         else:
+            input("Can't Handle headerless data sets yet")
+            exit()
             return(list(filter(lambda record: 
-                        record[self.get_header_index(header="StartDate")]!="",
+                        record[self.get_header_index("StartDate")]!="",
                             self.todo_file)))
     def new_entry(self):
         empty_row_index = self.get_record(record_id="max")+1
-        print("empty row index = ",empty_row_index)
-        input()
         def get_input():
             print('''
             The format for adding an item:
@@ -89,28 +77,72 @@ class TodoFile():
                 Default input order: Description, Category, Importance
                 Override defaults with shortcuts.''')
 
-            description = input('Task Description: ')
-            category = input('Category: ')
-            importance = input('Importance: ')
-            extra_info = input('Extra Information: ')
             return(description, category, importance, extra_info)
-        def insert_input(description, category, importance, extra_info):
-            f_new = open(self.fullpath, 'w')
-            for line in self.cleaned_file:
-                f_new.write("\t".join(line))
+        description, category, importance, extra_info = get_input()
+        self.insert_input(new_entry=True,
+                            description=description,
+                            category=category,
+                            importance=importance,
+                            extra_info=extra_info)
+
+    def insert_input(self, **kwargs):
+        f_new = open(self.fullpath, 'w')
+        f_new.write("\t".join(self.headers)+"\n")
+        for line in self.cleaned_file:
+            if "\n" not in line[-1]:
+                line[-1]+="\n"
+            f_new.write("\t".join(line))
+        if kwargs:
             f_new.write("\t".join(["\n"+str(empty_row_index),
-                                    importance,
-                                    datetime.datetime.now().isoformat()[:10],
-                                    category,
-                                    description,
-                                    extra_info,
+                                    kwargs["importance"],
+                                    self.current_date,
+                                    kwargs["category"],
+                                    kwargs["description"],
+                                    kwargs["extra_info"],
                                     "x",
                                     "x"]))
 
-            
 
-        description, category, importance, extra_info = get_input()
-        insert_input(description, category, importance, extra_info)
+    def current_date(self):
+        return datetime.datetime.now().isoformat()[:10]
+
+    def get_record(self, record_id=False):
+        if not self.cleaned_file:
+            self.cleaned_file = self.clean_file()
+            print(self.cleaned_file)
+        if record_id != "max":
+            for record in self.cleaned_file:
+                if int(record[self.get_header_index("ID")])==int(record_id):    
+                    return(record)                 
+            
+            #return(therecord) #a list --maybe in a list? :(
+        else: #if record_id = "max"
+            ''' Returns int(max_record_id)'''
+            max_record_id = list(map(lambda record: 
+                int(record[self.get_header_index("ID")]),
+                    self.cleaned_file))
+            print("max_rec_id: ",max_record_id)
+            return(int(max(max_record_id)))
+
+    def close_item(self, purpose="close",record_id=False):
+        '''
+            Almost works, but some index errors and list incomprehensibles
+        '''
+        def update_that_line(index):
+            self.cleaned_file[index][self.get_header_index("CompletionDate")] = \
+                                                        self.current_date()
+            return(self.cleaned_file[index])
+        #record = self.get_record(record_id=record_id)
+        if not self.cleaned_file:
+            self.cleaned_file = self.clean_file()
+        for index, row in enumerate(self.cleaned_file):
+            if record_id == row[self.get_header_index("ID")]:
+                verify = input("Are you sure you'd like to mark %s as \
+                    completed on todays date? [y\\n] \n Response: " % self.cleaned_file[index])
+                if verify.lower().strip() in ["y","yes"]:
+                    update_that_line(index)
+        self.insert_input() 
+
 
 
     def sort():
--- todo-more/run.py
+++ todo-more/run.py
@@ -1,7 +1,12 @@
 #run.py
 import TodoFile
 f = TodoFile.TodoFile()
-if input("Enter Mode: ") != "run":
-    f.get_record(record_id="max")
-else:
+mode = input("Enter Mode: ")
+if mode =="":
+    print(f.get_record(record_id="max") )
+elif "c" in mode:
+    record_id = mode.split(' ')[1]
+    print(record_id)
+    f.close_item(record_id=record_id)
+elif mode == "run":
     f.new_entry()
